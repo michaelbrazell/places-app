72a6e8bbb29ddf278546096141a4a6d1f306d612
Mike Murray <michaelbrazell@gmail.com>
2017-04-07 13:12:38 -0400

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+
+var Main = (props) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="columns medium-6 large-4 small-centered">
+          <p>Main.jsx Rendered</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+module.exports = Main;
